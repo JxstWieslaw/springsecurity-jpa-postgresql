cb93bbd8d95fb4554ad94893232ab3f6fcb955fc
JxstWieslaw <wieslawsamushonga01@gmail.com>
2022-02-05 15:22:10 +0530

    Initial Commit

---
+++ src/main/java/ml/codevilla/springsecurityjpapostgresql/SpringsecurityJpaPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package ml.codevilla.springsecurityjpapostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityJpaPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityJpaPostgresqlApplication.class, args);
+	}
+
+}
