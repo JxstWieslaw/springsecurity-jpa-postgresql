6482b4bf4f5c85ec78bfb9e0b72749f0264a6e00
JxstWieslaw <wieslawsamushonga01@gmail.com>
2022-02-06 02:42:27 +0530

    User @Entity @Table UserRepository @Repository with findUserByUserName(String userName) @EnableJpaRepositories in Spring Context MyUserDetails implements UserDetails MyUserDetailsService implements UserDetailsService @Service

--- src/main/java/ml/codevilla/springsecurityjpapostgresql/MyUserDetailsService.java
+++ src/main/java/ml/codevilla/springsecurityjpapostgresql/MyUserDetailsService.java
@@ -1,15 +1,26 @@
 package ml.codevilla.springsecurityjpapostgresql;
 
+import ml.codevilla.springsecurityjpapostgresql.models.MyUserDetails;
+import ml.codevilla.springsecurityjpapostgresql.models.User;
+import ml.codevilla.springsecurityjpapostgresql.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new MyUserDetails(username);
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findUserByUserName(userName);
+        user.orElseThrow(()->new UsernameNotFoundException("Not found: "+userName));
+        return user.map(MyUserDetails::new).get();
     }
 }
--- src/main/java/ml/codevilla/springsecurityjpapostgresql/SecurityConfiguration.java
+++ src/main/java/ml/codevilla/springsecurityjpapostgresql/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package ml.codevilla.springsecurityjpapostgresql;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -26,5 +29,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .and()
                 .formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
